6244f92e20a9f0deede5899a3489443a43bac1e9
Erik Engheim <erik.engheim@mac.com>
2015-06-14 00:18:31 +0200

    Initial Commit

---
+++ Streng/Streng.h
@@ -0,0 +1,19 @@
+//
+//  Streng.h
+//  Streng
+//
+//  Created by Erik Engheim on 14/06/15.
+//  Copyright © 2015 Translusion. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for Streng.
+FOUNDATION_EXPORT double StrengVersionNumber;
+
+//! Project version string for Streng.
+FOUNDATION_EXPORT const unsigned char StrengVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Streng/PublicHeader.h>
+
+
